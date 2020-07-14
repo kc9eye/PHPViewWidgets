34868a48e33ee7f8d3dd0675f8b2da88956ece84
Paul Lane <kc9eye@outlook.com>
2020-07-14 07:44:52 -0500

    docs and widgets

---
+++ widgets/uiheading.php
@@ -0,0 +1,54 @@
+<?php
+//File: uiheading.php
+//
+//About: License
+//
+//Copyright (C)2020 Paul W. Lane <kc9eye@gmail.com>
+//
+//This program is free software; you can redistribute it and/or modify
+//
+//it under the terms of the GNU General Public License as published by
+//
+//the Free Software Foundation; version 2 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along
+//
+//with this program; if not, write to the Free Software Foundation, Inc.
+//
+//51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//namespace: PHPViewWidgets\Widgets
+namespace PHPViewWidgets\Widgets {
+    //class: PHPViewWidgets\Widgets\UIHeading
+    //This class represents a heading widget.
+    //
+    //topic: Extends
+    //<PHPViewWidgets\Widgets\UIWidget>
+    //
+    //topic: Options
+    //This widget accepts the following options:
+    //
+    //heading - The string heading.
+    //size - The optional integer denoting the heading size (default is 1, the largest)
+    class UIHeading extends UIWidget {
+        public function __construct(Options $opts = null) {
+            parent::__construct($opts);
+        }
+
+        public function ToString() {
+            $size = isset($this->opts->size) ? "{$this->opts->size}>" : "1>";
+            $this->out .= "<h{$size}>";
+            $this->out .= isset($this->opts->heading) ? "{$this->opts->heading}" : "";
+            $this->out .= "</h{$size}>";
+            return $this->out;
+        }
+    }
+}

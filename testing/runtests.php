2a1619818bf1e829f531ad4eccd432a972bbf552
Paul Lane <kc9eye@outlook.com>
2020-08-06 15:32:58 -0500

    update

--- testing/runtests.php
+++ testing/runtests.php
@@ -25,59 +25,61 @@
 //
 //51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-use PHPViewWidgets\Templates\BS4Body;
-use PHPViewWidgets\Templates\BS4Col;
-use PHPViewWidgets\Templates\BS4Head;
-use PHPViewWidgets\Templates\BS4Row;
-use PHPViewWidgets\Templates\DataGrid;
-use PHPViewWidgets\Templates\GridData;
-use PHPViewWidgets\Widgets\Options;
-use PHPViewWidgets\Widgets\UIHeading;
-use PHPViewWidgets\Widgets\UIHtml;
-use PHPViewWidgets\Widgets\UITitle;
+// use PHPViewWidgets\Templates\BS4Body;
+// use PHPViewWidgets\Templates\BS4Col;
+// use PHPViewWidgets\Templates\BS4Head;
+// use PHPViewWidgets\Templates\BS4Row;
+// use PHPViewWidgets\Templates\DataGrid;
+// use PHPViewWidgets\Templates\GridData;
+// use PHPViewWidgets\Widgets\Options;
+// use PHPViewWidgets\Widgets\UIHeading;
+// use PHPViewWidgets\Widgets\UIHtml;
+// use PHPViewWidgets\Widgets\UITitle;
 
-require_once("PHPViewWidgets/autoloader.php");
+// require_once("PHPViewWidgets/autoloader.php");
 
-$data = new GridData([
-    ['col1'=>'row1-1','col2'=>'row1-2','col3'=>"row1-3"],
-    ['col1'=>'row2-1','col2'=>'row2-2','col3'=>"row2-3"],
-    ['col1'=>'row3-1','col2'=>'row3-2','col3'=>'row3-3']
-]);
+// $data = new GridData([
+//     ['col1'=>'row1-1','col2'=>'row1-2','col3'=>"row1-3"],
+//     ['col1'=>'row2-1','col2'=>'row2-2','col3'=>"row2-3"],
+//     ['col1'=>'row3-1','col2'=>'row3-2','col3'=>'row3-3']
+// ]);
 
-$head = new BS4Head(null, [new UITitle(new Options(['title'=>"Testing Grid"]))]);
-$body = new BS4Body(
-    new Options(['class'=>"bg-dark text-light"]),
-    [
-        new UIHeading(new Options(['heading'=>"BS4 Grid Testing"])),
-        new BS4Row(
-            null,
-            [
-                new BS4Col(
-                    new Options(['class'=>"bg-light text-dark m1"]),
-                    [
-                        new UIHeading(new Options(['heading'=>"Column 1", 'size'=>3])),
-                        new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
-                    ]
-                ),
-                new BS4Col(
-                    new Options(['class'=>"bg-light text-dark m1"]), 
-                    [
-                        new UIHeading(new Options(['heading'=>"Column 2", 'size'=>3])),
-                        new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
-                    ]
-                ),
-                new BS4Col(
-                    new Options(['class'=>"bg-light text-dark m1"]),
-                    [
-                        new UIHeading(new Options(['heading'=>"Column 3", 'size'=>3])),
-                        new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
-                    ]
-                )
-            ]
-        )
-    ]
-);
+// $head = new BS4Head(null, [new UITitle(new Options(['title'=>"Testing Grid"]))]);
+// $body = new BS4Body(
+//     new Options(['class'=>"bg-dark text-light"]),
+//     [
+//         new UIHeading(new Options(['heading'=>"BS4 Grid Testing"])),
+//         new BS4Row(
+//             null,
+//             [
+//                 new BS4Col(
+//                     new Options(['class'=>"bg-light text-dark m1"]),
+//                     [
+//                         new UIHeading(new Options(['heading'=>"Column 1", 'size'=>3])),
+//                         new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
+//                     ]
+//                 ),
+//                 new BS4Col(
+//                     new Options(['class'=>"bg-light text-dark m1"]), 
+//                     [
+//                         new UIHeading(new Options(['heading'=>"Column 2", 'size'=>3])),
+//                         new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
+//                     ]
+//                 ),
+//                 new BS4Col(
+//                     new Options(['class'=>"bg-light text-dark m1"]),
+//                     [
+//                         new UIHeading(new Options(['heading'=>"Column 3", 'size'=>3])),
+//                         new DataGrid(['table'=>new Options(['class'=>"text-light bg-dark"])], $data, ["Column 1","Column 2","Column 3"])
+//                     ]
+//                 )
+//             ]
+//         )
+//     ]
+// );
 
-$html = new UIHtml(null,[$head,$body]);
+// $html = new UIHtml(null,[$head,$body]);
 
-$html->Display();
+// $html->Display();
+
+echo "<h1>It Works, Docker</h1>";

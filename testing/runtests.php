4d23b70f170115da527a73caf2d2c1fb46a4a14e
Paul Lane <kc9eye@outlook.com>
2020-08-10 07:36:16 -0500

    paths

--- testing/runtests.php
+++ testing/runtests.php
@@ -25,4 +25,4 @@
 //
 //51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-require_once(dirname(diranme(__FILE__))."/autoloader.php");
+require_once(dirname(dirname(__FILE__))."/autoloader.php");

9386aa399291f6dd8009974c5482344a58f23a56
Paul W. Lane <kc9eye@outlook.com>
2020-09-24 10:20:27 -0500

    moved scripts to end of body

--- templates/bs4head.php
+++ templates/bs4head.php
@@ -59,19 +59,7 @@ namespace PHPViewWidgets\Templates {
                     'rel'=>"stylesheet",
                     'href'=>"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
                     'other'=>"integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk' crossorigin='anonymous'"
-                ])),
-                new UIScript(new Options([
-                    'src'=>"https://code.jquery.com/jquery-3.5.1.slim.min.js",
-                    'other'=>"integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'"
-                ])),
-                new UIScript(new Options([
-                    'src'=>"https://unpkg.com/@popperjs/core@2"
-                    
-                ])),
-                new UIScript(new Options([
-                    'src'=>"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
-                    'other'=>"integrity='sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI' crossorigin='anonymous'"
-                ]))
+                ]))                
             ];
             if (!empty($widgets)) {
                 foreach($widgets as $w) {

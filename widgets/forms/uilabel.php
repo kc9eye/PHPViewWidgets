23ed9eca7236c57ecda2d6cbd5d9f0b1d2ea2773
Paul W. Lane <kc9eye@outlook.com>
2020-10-26 12:18:19 -0500

    added uilabel

---
+++ widgets/forms/uilabel.php
@@ -0,0 +1,55 @@
+<?php
+//File: uilabel.php
+//
+//About: License
+//
+//Copyright (C)2020 Paul W. Lane <kc9eye@gmail.com>
+//
+//This program is free software; you can redistribute it and/or modify
+//
+//it under the terms of the GNU General Public License as published by
+//
+//the Free Software Foundation; version 2 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along
+//
+//with this program; if not, write to the Free Software Foundation, Inc.
+//
+//51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//namespace: PHPViewWidgets\Widgets\Forms
+namespace PHPViewWidgets\Widgets\Forms
+{
+    use PHPViewWidgets\Widgets\Options;
+    use PHPViewWidgets\Widgets\UIWidget;
+
+    class UILabel extends UIWidget
+    {
+        public function __construct(Options $opts = null)
+        {
+            parent::__construct($opts);
+        }
+
+        public function ToString()
+        {
+            $this->out .= "<label";
+            $this->out .= isset($this->opts->for) ? " for='{$this->opts->for}'" : "";
+            $this->out .= isset($this->opts->class) ? " class='{$this->opts->class}'" : "";
+            $this->out .= isset($this->opts->style) ? " style='{$this->opts->style}'" : "";
+            $this->out .= isset($this->opts->id) ? " id='{$this->opts->id}'" : "";
+            $this->out .= isset($this->opts->other) ? " {$this->opts->other}" : "";
+            $this->out .= ">";
+            $this->out .= isset($this->opts->label) ? "{$this->opts->label}" : "";
+            $this->out .= "</label>";
+            return $this->out;
+        }
+    }
+}
